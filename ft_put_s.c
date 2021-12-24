fa2b94b4ee8dc34075a4186a74f4c4cbd1804da7
Ismail Raqi <iraqi@e1r2p2.1337.ma>
2021-12-24 16:15:42 +0100

    putstr null cases fixed

--- ft_put_s.c
+++ ft_put_s.c
@@ -6,7 +6,7 @@
 /*   By: iraqi <iraqi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 21:50:24 by iraqi             #+#    #+#             */
-/*   Updated: 2021/12/24 02:22:58 by iraqi            ###   ########.fr       */
+/*   Updated: 2021/12/24 15:58:27 by iraqi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,10 @@ void	ft_put_s(va_list args_list, int *ret_value)
 	char	*str;
 
 	str = (char *) va_arg(args_list, char *);
+	if (!str)
+	{
+		ft_putstr("(null)", ret_value);
+		return ;
+	}
 	ft_putstr(str, ret_value);
 }
